8168af1d8b8cdb5bbb6293cf9b4e3c71a22edf1b
laurent-lezardon <59555017+laurent-lezardon@users.noreply.github.com>
2022-11-18 10:21:30 +0100

    Init Projet

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+
+
+
+
+mongoose.connect('mongodb+srv://lezardon:Aaqwzsxedc0@lezardon.p1ktrcv.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/', (req,res,next) => {
+    res.status(200).json({message : "je vous ai entendu !"})
+})
+
+
+
+
+
+module.exports = app
