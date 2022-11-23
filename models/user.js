7bb689b5882e1fb7b239e90cef46e4b75afee842
laurent-lezardon <59555017+laurent-lezardon@users.noreply.github.com>
2022-11-23 12:33:03 +0100

    Suppression images et .env sur github (.gitignore)
    Pull des modifications et création de .env et des fichiers images via le frontback

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 const uniqueValidator = require("mongoose-unique-validator")
 
+
+// création du Schema Uer
 const userSchema = mongoose.Schema({
     email : {type:String, required:true, unique: true},
     password : {type:String, required:true}
