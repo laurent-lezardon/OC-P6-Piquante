89edc1d2e4ae79cb0ca4156e3886a21d6f6b9c62
laurent-lezardon <59555017+laurent-lezardon@users.noreply.github.com>
2022-11-23 11:45:29 +0100

    contrôleur sauce : modifySauce
    configuration de modifySauce avec suppression de l'ancien fichier si l'image est changée

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const sauce = require("../models/sauce")
 
 
+
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => {
@@ -11,6 +12,7 @@ exports.getAllSauces = (req, res, next) => {
         })
         .catch((error) => res.status(400).json({ error }))
 }
+
 
 exports.createSauce = (req, res, next) => {
     // console.log(req.body.sauce)
@@ -41,36 +43,107 @@ exports.getOneSauce = (req, res, next) => {
             res.status(200).json(sauce)
         }
         )
-    
 }
-exports.modifySauce = (req, res, next) => {
-    res.status(200).json({ message: "modifySauce !" })
-}
+
+
+
+
+
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then((sauce) => {
             console.log(sauce)
             console.log(sauce.userId, req.auth.userId)
             if (sauce.userId != req.auth.userId) {
-                res.status(401).json({message : 'Not Authorized'})
+                res.status(401).json({ message: 'Not Authorized' })
             } else {
-                
+                const sauceImageUrl = sauce.imageUrl
                 const filename = sauce.imageUrl.split('/images/')[1]
-                console.log(filename)
+                // console.log(filename)
                 fs.unlink(`images/${filename}`, () => {
-                    sauce.deleteOne({_id:req.params.id})
-                    .then(() => {
-                        res.status(200).json({message: 'Sauce supprimée !'})
-                    })
-                    .catch(error => res.status(401).json({error}))
+                    Sauce.deleteOne({ _id: req.params.id })
+                        .then(() => {
+                            res.status(200).json({ message: 'Sauce supprimée !' })
+                        })
+                        .catch(error => res.status(401).json({ error }))
                 })
+            }
+        }
+        )
+}
+
+
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+            // console.log("sauce modifiée : ",req.params.id,sauceObject)
+            // console.log("sauce BdD : ",sauce)
+            // res.status(200).json({ message: "modifySauce !" })
+
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Not authorized' });
+            } else if (req.file) {
+                // console.log(sauce.imageUrl)
+                const filename = sauce.imageUrl.split('/images/')[1]
+                fs.unlink(`images/${filename}`, () => {
+                    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+                        .catch(error => res.status(401).json({ error }));
+                })
+
+            } else {
+                Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+                    .catch(error => res.status(401).json({ error }));
+
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+}
+
+
+exports.modifySauceLike = (req, res, next) => {
+    const likeObject = req.body
+    console.log(likeObject.userId)
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+            console.log(likeObject.like)
+            switch (likeObject.like) {
+                case 1:
+                    console.log("like")
+                    console.log("avant : " ,sauce.usersLiked)
+                    sauce.usersLiked.push(likeObject.userId)
+                    console.log("après : " ,sauce.usersLiked)
+                    console.log(sauce)
+                    sauce.likes += 1
+
+                    Sauce.updateOne({ _id: sauce._id }, { usersLiked : sauce.usersLiked,likes : sauce.likes })
+                        .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+                        .catch(error => res.status(401).json({ error }));
+
+                    break
+                case -1:
+                    console.log("unlike")
+                    res.status(200).json({ message: "modifySauce like !" })
+                    break
+                case 0:
+                    console.log("changé d'avis")
+                    res.status(200).json({ message: "modifySauce like !" })
+                    break
+
+                default:
+                    console.log("pas like")
+                    res.status(200).json({ message: "modifySauce like !" })
             }
             
         }
         )
-
-    
-}
-exports.modifySauceLike = (req, res, next) => {
-    res.status(200).json({ message: "modifySauceLike !" })
 }
--- routes/sauce.js
+++ routes/sauce.js
@@ -7,7 +7,7 @@ const multer = require("../middleware/multer-config")
 router.get('/', auth, sauceCtrl.getAllSauces)
 router.post('/', auth, multer,sauceCtrl.createSauce)
 router.get('/:id', auth, sauceCtrl.getOneSauce)
-router.put('/:id', auth, sauceCtrl.modifySauce)
+router.put('/:id', auth, multer,sauceCtrl.modifySauce)
 router.delete('/:id', auth, sauceCtrl.deleteSauce)
 router.post('/:id/like', auth, sauceCtrl.modifySauceLike)
 
