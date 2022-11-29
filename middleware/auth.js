e5cd7c3ab7afe22dc31d3d5375cc123c8bc9bdd5
laurent-lezardon <59555017+laurent-lezardon@users.noreply.github.com>
2022-11-29 16:47:55 +0100

    Commentaires auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,15 @@
+// Module authentification
+// recupère le token dans l'entète de la requète, essaye de le décoder
+// avec la clé présente dans le fichier .env.  Insére alors la clé auth : userId
+// dans req et transmet au module suivant (next)
+
 const dotenv = require('dotenv')
 const jwt = require('jsonwebtoken')
 dotenv.config()
  
 module.exports = (req, res, next) => {
    try {
+    
        const token = req.headers.authorization.split(' ')[1];
        const decodedToken = jwt.verify(token, process.env.APP_SECRET);
        const userId = decodedToken.userId;
